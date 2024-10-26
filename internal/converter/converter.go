371b38f25cef7d875b1d45a731e42c8f801f6430
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-10-26 20:48:09 +0900

    EXIFを削除する関数を実装

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -1,13 +1,31 @@
 package converter
 
 import (
+	"bytes"
+	"errors"
 	"image"
 
-	"golang.org/x/image/draw"
+	"github.com/disintegration/imaging"
 )
 
 func ResizeImage(img image.Image, width int, height int) (image.Image, error) {
-	m := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.BiLinear.Scale(m, m.Bounds(), img, img.Bounds(), draw.Over, nil)
+	if width <= 0 || height <= 0 {
+		return nil, errors.New("Invalid width or height")
+	}
+	m := imaging.Resize(img, width, height, imaging.Lanczos)
+	return m, nil
+}
+
+func DeleteExif(img image.Image, format imaging.Format) (image.Image, error) {
+	r := new(bytes.Buffer)
+	if err := imaging.Encode(r, img, format); err != nil {
+		return nil, err
+	}
+
+	m, err := imaging.Decode(r, imaging.AutoOrientation(true))
+	if err != nil {
+		return nil, err
+	}
+
 	return m, nil
 }
