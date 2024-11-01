184fea7d8043519bbdd9dc31ff6008a706253de0
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-01 20:06:57 +0900

    見つからない場合は、nilを返すようにした

--- image-server/internal/service/image.go
+++ image-server/internal/service/image.go
@@ -11,5 +11,8 @@ func FindImageById(db *database.Database, id int64) (*model.Image, error) {
 	if res.Error != nil {
 		return nil, res.Error
 	}
+	if res.RowsAffected == 0 {
+		return nil, nil
+	}
 	return image, nil
 }
