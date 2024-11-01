a5ca436422d9d638dce79fb5a65d683ed918347b
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-01 17:02:28 +0900

    サンプルを書く

--- image-server/go.mod
+++ image-server/go.mod
@@ -1,3 +1,8 @@
 module github.com/rokuosan/image-server
 
 go 1.23.1
+
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	golang.org/x/image v0.21.0 // indirect
+)
--- image-server/main.go
+++ image-server/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+	"image"
+
+	"github.com/disintegration/imaging"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func ResizeImage(img image.Image, width int, height int) (image.Image, error) {
+	if width <= 0 || height <= 0 {
+		return nil, errors.New("Invalid width or height")
+	}
+	m := imaging.Resize(img, width, height, imaging.Lanczos)
+	return m, nil
+}
