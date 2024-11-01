ce7262e3b7fe90236ec69913ac059f3657b2c0a3
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-01 20:34:40 +0900

    image をIDから取るエンドポイントを生やした

--- image-server/internal/handler/image.go
+++ image-server/internal/handler/image.go
@@ -1,6 +1,10 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/rokuosan/image-server/internal/database"
+	"github.com/rokuosan/image-server/internal/model"
+)
 
 type ImageHandler struct{}
 
@@ -9,11 +13,23 @@ func NewImageHandler() *ImageHandler {
 }
 
 func (h *ImageHandler) Route() string {
-	return "/image"
+	return "/image/:id"
 }
 
 func (h *ImageHandler) GET(c echo.Context) error {
-	return c.String(200, "GET")
+	db := database.New()
+	id, err := ParamsInt64(c, "id")
+	if err != nil {
+		return c.JSON(400, map[string]string{"error": "Invalid ID"})
+	}
+
+	image := &model.Image{Id: id}
+	res := db.Find(image)
+	if res.Error != nil {
+		return c.JSON(500, map[string]string{"error": "Internal Server Error"})
+	}
+
+	return c.JSON(200, image)
 }
 
 func (h *ImageHandler) POST(c echo.Context) error {
