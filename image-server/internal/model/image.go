f0515a76d5edf79f3333dc76f815435bf8182893
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-01 20:06:37 +0900

    カラム名を指定する

--- image-server/internal/model/image.go
+++ image-server/internal/model/image.go
@@ -8,7 +8,7 @@ type Image struct {
 	Id int64 `json:"id"`
 
 	Content string `json:"content"`
-	Digest  string `json:"sha256_digest"`
+	Digest  string `json:"sha256_digest" gorm:"column:sha256_digest"`
 
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
