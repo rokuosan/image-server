af3b306a8a3ee592deeb23afd8ab2b8aa143aef9
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-01 18:47:41 +0900

    ImageのServiceクラスを生やした

---
+++ image-server/internal/service/image.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"github.com/rokuosan/image-server/internal/database"
+	"github.com/rokuosan/image-server/internal/model"
+)
+
+func FindImageById(db *database.Database, id int64) (*model.Image, error) {
+	image := &model.Image{Id: id}
+	res := db.Find(image)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return image, nil
+}
