2ab604acf37c16296e6c8e1454da8fe8c21f8545
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-01 21:28:38 +0900

    リサイズのクエリに対応した

--- image-server/internal/handler/content_view.go
+++ image-server/internal/handler/content_view.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/rokuosan/image-server/internal/database"
+	imageprocessor "github.com/rokuosan/image-server/internal/image-processor"
 	"github.com/rokuosan/image-server/internal/service"
 )
 
@@ -41,6 +42,31 @@ func (h *ContentViewHandler) GET(c echo.Context) error {
 	}
 	if image == nil {
 		return c.JSON(404, map[string]string{"error": "Not Found"})
+	}
+
+	// クエリパラメータsizeがあればリサイズする
+	size := c.QueryParam("size")
+	if size != "" {
+		w, h, err := imageprocessor.ParseSizeNxM(size)
+		if err != nil {
+			return c.JSON(400, map[string]string{"error": "Invalid Size"})
+		}
+
+		img, err := imageprocessor.Base64ToImage(image.Content)
+		if err != nil {
+			return c.JSON(500, map[string]string{"error": "Failed to convert base64 to image"})
+		}
+
+		img, err = imageprocessor.ResizeImage(img, w, h)
+		if err != nil {
+			return c.JSON(500, map[string]string{"error": "Failed to resize image"})
+		}
+
+		encoded, err := imageprocessor.EncodeImage(img, content.Extension)
+		if err != nil {
+			return c.JSON(500, map[string]string{"error": "Failed to encode image"})
+		}
+		return c.Blob(200, "image/"+content.Extension, encoded)
 	}
 
 	// Base64から画像に変換
